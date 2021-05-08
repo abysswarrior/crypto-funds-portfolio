4ab039afe030098c864e2a153c0cba4b1bf8807f
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-08 13:31:49 +0430

    feat(endpoint): home view added for panel app
    for now it's just a text

--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def home(request):
+    """panels home page"""
+    return HttpResponse("<h1>welcome to crypto funds portfolio</h1>")
