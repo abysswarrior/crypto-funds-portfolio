3125996a4409c839f6075ca722fdd4a85c50aa1c
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-08 11:47:29 +0430

    feat(app): binance lab portfolio added
    this app used for crawling messari.io binance lab portfolio

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common.apps.CommonConfig',
+    'binance_lab_portfolio.apps.BinanceLabPortfolioConfig',
 ]
 
 MIDDLEWARE = [
