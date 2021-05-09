da1cdd7d2e8753dc23f3c654dcc2bfd2c8ab2b46
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-09 23:43:46 +0430

    fix(UI): bootstrap spinner.gif problem

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
