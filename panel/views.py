3c8d6ccd702b286733f9c787f5fd9f5caad3086c
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-09 19:36:53 +0430

    feat(UI): bootstrap theme added to panel app

--- panel/views.py
+++ panel/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     """panels home page"""
-    return HttpResponse("<h1>welcome to crypto funds portfolio</h1>")
+    return render(request, 'panel/index.html')
