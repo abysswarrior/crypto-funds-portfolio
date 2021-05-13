2f41903d61c406c520b0b1916ff50c0505986ede
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-13 23:07:41 +0430

    feat(setting): add rest framework to installed app

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
     'common.apps.CommonConfig',
     'binance_lab_portfolio.apps.BinanceLabPortfolioConfig',
     'panel.apps.PanelConfig',
