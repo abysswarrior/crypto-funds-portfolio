72f43944f449f7f7c3aae96245206f18d62c4911
b1ng-b0ng <mehran.safaripour@gmail.com>
2021-05-14 22:43:45 +0430

    feat: add GOOGLE_CHROME_BIN env

--- config/environment_variables.py
+++ config/environment_variables.py
@@ -2,6 +2,8 @@ from decouple import config
 
 ON_HEROKU = config('ON_HEROKU', cast=bool)
 CHROMEDRIVER_PATH = config('CHROMEDRIVER_PATH')
+GOOGLE_CHROME_BIN = config('GOOGLE_CHROME_BIN', default=None)
+
 RESTRICTED_AREA = config('RESTRICTED_AREA', default=False, cast=bool)
 
 PROXY_HOST = config('PROXY_HOST', default=None)
