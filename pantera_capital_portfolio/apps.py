ee1cf10ea7fba5245371126ce24feb33691c03ea
promethe <mehran.safaripour@gmail.com>
2022-05-17 20:32:33 +0430

    feat(pantera_capital_portfolio): add new app

---
+++ pantera_capital_portfolio/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PanteraCapitalPortfolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pantera_capital_portfolio'
